336f8c18b37e9bacedd074af096ecc3acd534623
Willair <wmartinez.it@gmail.com>
2017-06-14 22:43:01 +1000

    jQuery testing

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-function hidehtmlcssgal () {
-	document.getElementsByClassName("htmlcss")[0].style.display="none";
-	document.getElementsByClassName("htmlcss")[1].style.display="none";
-	document.getElementsByClassName("jquery")[0].style.display="block";
-	document.getElementsByClassName("jquery")[1].style.display="block";
-}
 
-function hidejquerygal () {
-	document.getElementsByClassName("htmlcss")[0].style.display="block";
-	document.getElementsByClassName("htmlcss")[1].style.display="block";
-	document.getElementsByClassName("jquery")[0].style.display="none";
-	document.getElementsByClassName("jquery")[1].style.display="none";
-}
+$(document).ready(function() {
+	$("#htmlbtn").click(function() {
+		$(".html").fadeOut("slow");
+	});
+});
