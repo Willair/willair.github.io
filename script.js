97d148bff190a3d617fde1c831656597a0c9dd75
Willair <wmartinez.it@gmail.com>
2017-06-16 01:04:01 +1000

    Functional badges with jquery scripting

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 
 $(document).ready(function() {
-	$("#htmlbtn").click(function() {
-		$(".jquery").fadeOut("slow");
-		$(".html").fadeIn("slow");
-	});
+	$("#badges a").css("color", "white");	
+	$("#htmlbtn").css("background-color","#007acc");
 	
 	$("#allbtn").click(function() {
 		$(".jquery,.html").fadeIn("slow");
+		$("#allbtn").css("background-color","#007acc");
+		$("#jquerybtn,#htmlbtn,#bootstrapbtn,#responsivebtn").css("background-color","#3C352D");
+	});	
+	
+	$("#htmlbtn").click(function() {
+		$(".jquery").fadeOut("fast");
+		$(".html").fadeIn("slow");	
+		$("#htmlbtn").css("background-color","#007acc");
+		$("#jquerybtn,#allbtn,#bootstrapbtn,#responsivebtn").css("background-color","#3C352D");	
+	});
+	
+	$("#bootstrapbtn").click(function() {
+		$(".jquery,.html").fadeOut("fast");
+		$(".bootstrap").fadeIn("slow");	
+		$("#bootstrapbtn").css("background-color","#007acc");
+		$("#jquerybtn,#allbtn,#htmlbtn,#responsivebtn").css("background-color","#3C352D");	
 	});
 	
 	$("#jquerybtn").click(function() {
+		$(".html").fadeOut("fast");
 		$(".jquery").fadeIn("slow");
-		$(".html").fadeOut("slow");
+		$("#jquerybtn").css("background-color","#007acc");
+		$("#bootstrapbtn,#allbtn,#htmlbtn,#responsivebtn").css("background-color","#3C352D");			
+	});
+
+	$("#responsivebtn").click(function() {
+		$(".html,.jquery,.bootstrap").fadeOut("fast");
+		$(".responsive").fadeIn("slow");
+		$("#responsivebtn").css("background-color","#007acc");
+		$("#bootstrapbtn,#allbtn,#htmlbtn,#jquerybtn").css("background-color","#3C352D");			
 	});
 
 });
