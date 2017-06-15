4bf51b2dc148e9e4ced6a637dca0a6081552d5f7
Willair <wmartinez.it@gmail.com>
2017-06-15 22:28:09 +1000

    Gallery fadeins, profiles clean up, tags added.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
 $(document).ready(function() {
 	$("#htmlbtn").click(function() {
+		$(".jquery").fadeOut("slow");
+		$(".html").fadeIn("slow");
+	});
+	
+	$("#allbtn").click(function() {
+		$(".jquery,.html").fadeIn("slow");
+	});
+	
+	$("#jquerybtn").click(function() {
+		$(".jquery").fadeIn("slow");
 		$(".html").fadeOut("slow");
 	});
+
 });
