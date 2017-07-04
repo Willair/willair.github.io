4358f1b6ca1797355325e0e11427f0334a81ee29
Willair <wmartinez.it@gmail.com>
2017-07-05 07:41:21 +1000

    go

--- Pages/Dewars_Cleaning/app.js
+++ Pages/Dewars_Cleaning/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $("[data-toggle=popover]").popover();
 
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("a.smoothscrl").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
